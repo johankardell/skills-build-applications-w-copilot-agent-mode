a5c7b7aa6b17e8488e550c0460f06973e9975ab0
Johan Kardell <johan.kardell@gmail.com>
2025-04-08 13:06:29 +0000

    stage5

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -5,39 +5,28 @@ import Leaderboard from './components/Leaderboard';
 import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/activities">Activities</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/leaderboard">Leaderboard</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/teams">Teams</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/users">Users</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/workouts">Workouts</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Welcome to OctoFit Tracker</h1>
+          {/* Navigation menu */}
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/activities">Activities</a></li>
+              <li><a href="/leaderboard">Leaderboard</a></li>
+              <li><a href="/teams">Teams</a></li>
+              <li><a href="/users">Users</a></li>
+              <li><a href="/workouts">Workouts</a></li>
+            </ul>
+          </nav>
+        </header>
         <div className="mt-4">
           <Routes>
             <Route path="/activities" element={<Activities />} />
